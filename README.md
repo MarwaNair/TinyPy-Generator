fe444d47649faa10105f1daf73981de9167aa256
MarwaNair <jm_nair@esi.dz>
2024-01-02 06:12:10 -0500

    notebook to script

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Simple-Python-CFG-Generator
+
+In recent years, data has emerged as a valuable resource for creating intelligent systems. Procuring high-quality data, especially for code, remains challenging. To address this, we developed a tool that autonomously generates syntactically correct Python programs using context-free grammars.
+
+Our system establishes production rules that are recursively implemented to generate code. This process produces comprehensive code snippets with adjustable complexity, from basic assignments to advanced conditionals and loops. We validate correctness by executing the programs and capturing output. This enables effortless large-scale Python code generation, beneficial for applications like machine learning.
+
+Unlike existing research, we provide open-source implementation code. This allows customization according to user needs and extends potential usage to other languages.
+
+## How to Use
+
+### Command Line Arguments
+
+To use the code generator with different configurations, you can specify command line arguments. Here's how to add command line arguments:
+
+1. **num_programs**: Number of programs to generate and write (default is 10000).
+2. **level**: The level of the programs. Choose from:
+   - "1.1" for Simple arithmetic with a maximum of 3 variables.
+   - "1.2" for Advanced arithmetic with a maximum of 5 variables and enclosed expressions.
+   - "2.1" for Simple If-elif-else statements with a maximum of 2 variable conditions.
+   - "2.2" for Advanced If-elif-else statements with advanced arithmetic expressions and a maximum of 5 variables.
+   - "3.1" for Simple for loops with a maximum of 3 variables.
+   - "3.2" for Advanced for loops with a maximum of 5 variables and expressions everywhere.
+
+3. **filename**: Name of the file to write the programs (default is 'data.txt').
+
+### Example Usage
+
+Generate 5000 programs of level "3.1" and save them to a file named "output.txt":
+
+```bash
+python your_script.py --num_programs 5000 --level "3.1" --filename "output.txt"
+```
+
+Feel free to experiment with different values for `num_programs`, `level`, and `filename` to suit your requirements.
+
+```
+
+Feel free to modify and expand this template based on your specific requirements or additional information you want to provide.
---
+++ code_generator.py
@@ -0,0 +1,265 @@
+from anytree import Node, RenderTree
+import random
+from io import StringIO
+from contextlib import redirect_stdout
+import argparse  
+import time
+
+class CodeGenerator:
+    def __init__(self, cfg_rules):
+        """
+        Initialize the CodeGenerator object with the given context-free grammar rules.
+
+        Parameters:
+        - cfg_rules (dict): Dictionary containing context-free grammar rules.
+        """
+        self.cfg_rules = cfg_rules
+        self.init_count = 0
+        self.max_init = 0
+
+    def generate_code(self, symbol, assigned_identifiers, last_variable, parent=None):
+        """
+        Generate code recursively based on the context-free grammar rules.
+
+        Parameters:
+        - symbol (str): The symbol to generate code for.
+        - assigned_identifiers (set): Set of assigned identifiers.
+        - last_variable (set): Set of the last used variables.
+        - parent (Node): Parent node in the syntax tree.
+
+        Returns:
+        - str: The generated code.
+        """
+        node = Node(symbol, parent=parent)
+
+        if symbol in self.cfg_rules:
+            if symbol == "IDENTIFIER_INITIALIZATION":
+                if self.init_count < self.max_init:
+                    self.init_count += 1
+                else:
+                    symbol = "INITIALIZATION"
+
+            rule = random.choice(self.cfg_rules[symbol])
+            symbols = rule.split(" ")
+
+            generated_symbols = [self.generate_code(s, assigned_identifiers, last_variable, node) for s in symbols]
+
+            if symbol == "FINAL":
+                return str(eval(''.join(generated_symbols)))
+
+            if symbol == "INITIALIZATION":
+                assigned_identifiers.add(generated_symbols[0])
+
+            if (symbol == "SIMPLE_ASSIGNMENTS") or (symbol == "ADVANCED_ASSIGNMENTS"):
+                if generated_symbols[0]:
+                    last_variable.add(generated_symbols[0])
+
+            return ''.join(generated_symbols)
+
+        elif symbol == "EXPRESSION_IDENTIFIER":
+            identifier = random.choice(tuple(assigned_identifiers)) if assigned_identifiers else random.choice(self.cfg_rules["DIGIT"])
+            return identifier
+
+        elif symbol == "DISPLAY_IDENTIFIER":
+            try:
+                return f"{tuple(last_variable)[0]}"
+            except:
+                return f"{random.choice(tuple(assigned_identifiers))}"
+        else:
+            return symbol
+
+    def print_tree(self, root):
+        """
+        Print the syntax tree using the RenderTree utility from the anytree module.
+
+        Parameters:
+        - root (Node): The root node of the syntax tree.
+        """
+        for pre, _, node in RenderTree(root):
+            print(f"{pre}{node.name}")
+
+    def generate_program(self, level):
+        """
+        Generate a program based on the specified level.
+
+        Parameters:
+        - level (str): The level of the program.
+
+        Returns:
+        - Tuple[Node, str]: The syntax tree root node and the generated program.
+        """
+        assigned = set()
+        last_variable = set()
+        root = Node("ROOT")
+
+        self.init_count = 0
+        if level == "1.1":
+            self.max_init = 1
+        elif level == "1.2":
+            self.max_init = 3
+        elif level == "3.1":
+            self.max_init = 2
+        elif level == "3.2":
+            self.max_init = 4
+        else:
+            self.max_init = 5
+
+        program = self.generate_code("LEVEL" + level, assigned, last_variable, root)
+
+        return root, program.replace("SPACE", " ")
+
+    def generate_and_write_programs(self, num_programs, level, filename='data.txt'):
+        """
+        Generate and write a specified number of programs to a file.
+
+        Parameters:
+        - num_programs (int): Number of programs to generate and write.
+        - level (str): The level of the programs.
+        - filename (str): Name of the file to write the programs (default is 'data.txt').
+        """
+        start_time = time.time()  # Start time
+
+        with open(filename, 'w') as file:
+            for _ in range(num_programs):
+                try:
+                    root, program = self.generate_program(level)
+                    code = program + "\n# output"
+
+                    SIO = StringIO()
+                    with redirect_stdout(SIO):
+                        exec(code)
+                    output = SIO.getvalue().strip()
+
+                    output = '\n'.join([f'# {line}' if line else f'# ' for line in output.split('\n')])
+                    result = f"""{code}\n{output}"""
+                    file.write(result + '\n\n')
+                except Exception as e:
+                    continue
+
+        end_time = time.time()
+        print(f"Time taken: {end_time - start_time} seconds")
+
+# Context-free grammar rules    
+        
+cfg_rules = {
+    # Variables and digits
+    "VARIABLE": ["a", "b", "c", "d", "e",
+#                  "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+                ],
+    "DIGIT": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    
+    # Operators
+    "ARITHMETIC_OPERATOR": ["+", "-", "*", "/"],
+    "RELATIONAL_OPERATOR": ["<", ">", "<=", ">=", "!=", "=="],
+    "LOGICAL_OPERATOR_INFIX": ["and", "or"],
+    "LOGICAL_OPERATOR_PREFIX": ["not"],
+    "LOGICAL_OPERATOR": ["LOGICAL_OPERATOR_INFIX", "LOGICAL_OPERATOR_PREFIX"],
+    "OPERATOR": ["ARITHMETIC_OPERATOR"], 
+ 
+    # Formatting
+    "NEW_LINE": ["\n"],
+    "TAB_INDENT": ["\t"],
+    "BRACKET_OPEN": ['('],
+    "BRACKET_CLOSE": [')'],
+    "EQUALS": ["="],
+    "COLON": [":"],
+    "COMMA": [","],
+
+    
+    # Keywords
+    "IF": ["if"],
+    "ELIF": ["elif"],
+    "ELSE": ["else"],
+    "FOR": ["for"],
+    "IN": ["in"],
+    "RANGE": ["range"],
+    "WHILE": ["while"],
+    "PRINT": ["print"],
+
+    # Terms and expressions
+    "TERM": ["EXPRESSION_IDENTIFIER", "DIGIT"],
+    "EXPRESSION": ["TERM SPACE OPERATOR SPACE TERM"],
+    "ENCLOSED_EXPRESSION": ["BRACKET_OPEN EXPRESSION BRACKET_CLOSE"],
+    "DISPLAY_EXPRESSION": ["EXPRESSION_IDENTIFIER SPACE OPERATOR SPACE EXPRESSION_IDENTIFIER" ,
+                            "EXPRESSION_IDENTIFIER SPACE OPERATOR SPACE DIGIT"],
+    
+    # Initializations and assignments
+    "IDENTIFIER_INITIALIZATION": ["IDENTIFIER_INITIALIZATION INITIALIZATION", 
+                                  "INITIALIZATION"],
+
+    "INITIALIZATION": ["VARIABLE SPACE EQUALS SPACE DIGIT NEW_LINE"],
+    
+    "SIMPLE_ASSIGNMENTS": ["VARIABLE SPACE EQUALS SPACE EXPRESSION NEW_LINE" , ""],
+    "ADVANCED_ASSIGNMENTS": ["VARIABLE SPACE EQUALS SPACE SIMPLE_ARITHMETIC_EVALUATION NEW_LINE", 
+                             "VARIABLE SPACE EQUALS SPACE EXPRESSION NEW_LINE" , 
+                             ""],
+    
+    "SIMPLE_ARITHMETIC_EVALUATION": ["SIMPLE_ARITHMETIC_EVALUATION ARITHMETIC_OPERATOR ENCLOSED_EXPRESSION", 
+                                     "ENCLOSED_EXPRESSION",
+                                    ], 
+
+    # Conditions
+    "SIMPLE_IF_STATEMENT": ["IF SPACE CONDITION SPACE COLON NEW_LINE"],
+    "ADVANCED_IF_STATEMENT": ["IF SPACE CHAIN_CONDITION SPACE COLON NEW_LINE"],
+    "SIMPLE_ELIF_STATEMENT": ["ELIF SPACE CONDITION SPACE COLON NEW_LINE"],
+    "ADVANCED_ELIF_STATEMENT": ["ELIF SPACE CHAIN_CONDITION SPACE COLON NEW_LINE"],
+    "ELSE_STATEMENT": ["ELSE SPACE COLON NEW_LINE"],
+    
+    "CHAIN_CONDITION": ["CHAIN_CONDITION SPACE LOGICAL_OPERATOR_INFIX SPACE ENCLOSED_CONDITION", 
+                        "LOGICAL_OPERATOR_PREFIX SPACE ENCLOSED_CONDITION", 
+                        "ENCLOSED_CONDITION"],
+    "ENCLOSED_CONDITION": ["BRACKET_OPEN CONDITION BRACKET_CLOSE"],
+    "CONDITION": ["OPTIONAL_NOT CONDITION_EXPRESSION", "CONDITION_EXPRESSION"],
+    "CONDITION_EXPRESSION": ["EXPRESSION_IDENTIFIER SPACE RELATIONAL_OPERATOR SPACE EXPRESSION_IDENTIFIER", 
+                             "EXPRESSION_IDENTIFIER SPACE RELATIONAL_OPERATOR SPACE DIGIT"],
+    "OPTIONAL_NOT": ["LOGICAL_OPERATOR_PREFIX SPACE", "SPACE"], 
+
+    # Loops
+    "FOR_HEADER": ["FOR SPACE EXPRESSION_IDENTIFIER SPACE IN SPACE RANGE BRACKET_OPEN INITIAL COMMA SPACE FINAL COMMA SPACE STEP BRACKET_CLOSE SPACE COLON", 
+                   "FOR SPACE EXPRESSION_IDENTIFIER SPACE IN SPACE RANGE BRACKET_OPEN INITIAL COMMA SPACE FINAL BRACKET_CLOSE SPACE COLON"],
+    "INITIAL": ["DIGIT"],
+    "FINAL": ["STEP * EXECUTION_COUNT + INITIAL - 1"],
+    "STEP": ["1", "2", "3"],
+    "EXECUTION_COUNT": [ "2", "3"],
+    "FOR_LOOP": ["FOR_HEADER NEW_LINE TAB_INDENT DISPLAY"],
+    "ADVANCED_FOR_LOOP": ["FOR_LOOP",
+                          "FOR_HEADER NEW_LINE TAB_INDENT ADVANCED_DISPLAY"],
+    
+    
+    # Displaying 
+    "DISPLAY" : ["PRINT BRACKET_OPEN DISPLAY_IDENTIFIER BRACKET_CLOSE"],
+    "ADVANCED_DISPLAY" : ["DISPLAY",
+                          "PRINT BRACKET_OPEN DISPLAY_EXPRESSION BRACKET_CLOSE"],
+
+    
+    "LEVEL1.1": ["IDENTIFIER_INITIALIZATION SIMPLE_ASSIGNMENTS ADVANCED_DISPLAY"],
+    "LEVEL1.2": ["IDENTIFIER_INITIALIZATION ADVANCED_ASSIGNMENTS ADVANCED_DISPLAY"],
+    "LEVEL2.1": ["IDENTIFIER_INITIALIZATION SIMPLE_IF_STATEMENT TAB_INDENT DISPLAY", 
+                "IDENTIFIER_INITIALIZATION SIMPLE_IF_STATEMENT TAB_INDENT DISPLAY NEW_LINE SIMPLE_ELIF_STATEMENT TAB_INDENT DISPLAY NEW_LINE ELSE_STATEMENT TAB_INDENT DISPLAY", 
+                "IDENTIFIER_INITIALIZATION SIMPLE_IF_STATEMENT TAB_INDENT DISPLAY NEW_LINE ELSE_STATEMENT TAB_INDENT DISPLAY"],
+    "LEVEL2.2": ["IDENTIFIER_INITIALIZATION ADVANCED_ASSIGNMENTS ADVANCED_IF_STATEMENT TAB_INDENT ADVANCED_DISPLAY", 
+                "IDENTIFIER_INITIALIZATION ADVANCED_ASSIGNMENTS ADVANCED_IF_STATEMENT TAB_INDENT ADVANCED_DISPLAY NEW_LINE ADVANCED_ELIF_STATEMENT TAB_INDENT ADVANCED_DISPLAY NEW_LINE ELSE_STATEMENT TAB_INDENT ADVANCED_DISPLAY", 
+                "IDENTIFIER_INITIALIZATION ADVANCED_ASSIGNMENTS ADVANCED_IF_STATEMENT TAB_INDENT ADVANCED_DISPLAY NEW_LINE ELSE_STATEMENT TAB_INDENT ADVANCED_DISPLAY"],
+    "LEVEL3.1": ["IDENTIFIER_INITIALIZATION FOR_LOOP"],
+    "LEVEL3.2": ["IDENTIFIER_INITIALIZATION ADVANCED_ASSIGNMENTS ADVANCED_FOR_LOOP"],
+
+ 
+}
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate and write programs based on a specified level. ')
+    parser.add_argument('--num_programs', type=int, default=1000, help='Number of programs to generate and write (default is 1000)')
+    parser.add_argument('--level', required=True, help='The level of the programs (1.1, 1.2, 2.1, 2.2, 3.1, 3.2)')
+    parser.add_argument('--filename', default='data.txt', help='Name of the file to write the programs (default is data.txt)')
+
+    args = parser.parse_args()
+
+    # Create CodeGenerator instance and generate programs
+    code_generator = CodeGenerator(cfg_rules)
+    code_generator.generate_and_write_programs(num_programs=args.num_programs, level=args.level, filename=args.filename)
+
+if __name__ == "__main__":
+    main()

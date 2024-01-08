11b98979d485e4d025fa641a73bba1bce82dfb45
Kamel <jm_yamani@esi.dz>
2024-01-08 07:32:19 -0500

    Add General Level called : ALL, Add max tries for deduplication, Saving hash instead of all program when deduplicating

--- code_generator.py
+++ code_generator.py
@@ -21,8 +21,7 @@ class CodeGenerator:
         # Dictionary containing context-free grammar rules.
         self.cfg_rules = {
                 # Variables and digits
-                "VARIABLE": ["a"
-#                              , "b", "c", "d", "e",
+                "VARIABLE": ["a", "b", "c", "d", "e",
 #                                          "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
                             ],
                 "DIGIT": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
